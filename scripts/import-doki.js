c132bb963388f81439486f57e7000ee96d323d48
chihaoyo <chihaoyo@gmail.com>
2021-07-12 20:39:59 +0800

    Finally add import-doki script (originally download.js)

---
+++ scripts/import-doki.js
@@ -0,0 +1,73 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const fs = require('fs')
+const path = require('path')
+const { GoogleSpreadsheet } = require('google-spreadsheet')
+const { google } = require('googleapis')
+const drive = google.drive('v3')
+
+function ok(str) {
+  return str !== null && str !== undefined && str.trim().length > 0
+}
+
+const doc = new GoogleSpreadsheet(process.env.DA_DOC_ID)
+doc.useApiKey(process.env.GOOGLE_SHEET_API_KEY)
+
+async function downloadDriveFile(driveID, destFileName) {
+  console.info('Download file', driveID, 'to', destFileName)
+  const stream = await drive.files.get({ fileId: driveID, alt: 'media' }, {responseType: 'stream'})
+
+  let type = stream.headers['content-type'].split('/')[1]
+  if(type === 'jpeg'){
+    type = 'jpg'
+  } else if(!type) {
+    type = 'txt'
+  }
+  destFileName += '.' + type
+
+  const dest = fs.createWriteStream(path.join(__dirname, '../stage-import-doki' destFileName))
+  stream.data.pipe(dest)
+
+  return destFileName
+}
+
+async function importDoki() {
+  console.info('Import files from Dokidoki Archive...')
+  await doc.loadInfo()
+
+  let rows = await doc.sheetsById[process.env.DA_SHEET_ID].getRows()
+  rows = rows.filter(row => row.ioid && row.mainFile).map(row => ({
+    ioid: row.ioid,
+    mailFile: row.mainFile
+  }))
+  console.info('Total', rows.length, 'rows')
+
+  const auth = new google.auth.GoogleAuth({
+    keyFile: path.join(__dirname, process.env.KEY_PATH),
+    scopes: ['https://www.googleapis.com/auth/drive.readonly']
+  })
+  google.options({ auth })
+
+  const list = require('./import-doki-imported.json')
+  console.info(list.length, 'files already imported')
+  let newImportCount = 0
+
+  for(let i = 0; i < rows.length; i++) {
+    const row = rows[i]
+    const ioid = row.ioid
+    const driveID = row.mailFile.split('?id=')[1]
+    if(!list.some(f => f.includes(ioid))) {
+      const fileName = await downloadDriveFile(driveID, ioid)
+      list.push(fileName)
+      newImportCount += 1
+    } else {
+      console.info(ioid, 'skipped')
+    }
+  }
+
+  console.info(newImportCount, 'new files imported')
+  console.info(list.length, 'total files imported')
+  fs.writeFileSync('import-doki-imported.json', JSON.stringify(list))
+}
+
+importDoki()
